82708d44d2aedebed41ef2335dc6a4f7ce670560
RolyYang <38229007+RolyYang@users.noreply.github.com>
2018-05-15 14:55:30 -0400

    Create plot.h

---
+++ plot.h
@@ -0,0 +1,6 @@
+#ifndef plot_h
+#define plot_h
+
+void plotTrend(const Vector* groupData, const int num_of_record, const string title);
+void plotStock(const Vector &price, const unsigned long num_of_record, string name, string title);
+#endif /* plot_h */
