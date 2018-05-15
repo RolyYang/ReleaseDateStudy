ff3a04ba98dac10d7f7b7606f05b2d91545c0570
RolyYang <38229007+RolyYang@users.noreply.github.com>
2018-05-15 14:58:30 -0400

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,51 @@
+#include "Vector.h"
+
+Vector operator+(const Vector& V,const Vector& W)
+{
+    unsigned long d = V.size();
+    Vector U(d);
+    for (int j=0; j<d; j++) U[j] = V[j] + W[j];
+    return U;
+}
+
+Vector operator-(const Vector& V,const Vector& W)
+{
+    unsigned long d = V.size();
+    Vector U(d);
+    for (int j=0; j<d; j++) U[j] = V[j] - W[j];
+    return U;
+}
+
+Vector operator/(const Vector& V,const Vector& W)
+{
+    unsigned long d = V.size();
+    Vector U(d);
+    for (int j=0; j<d; j++) U[j] = V[j] / W[j];
+    return U;
+}
+
+Vector operator/(const Vector& V, const int x)
+{
+    double y = double(x);
+    unsigned long d = V.size();
+    Vector U(d);
+    for (int j=0; j<d; j++) U[j] = V[j] / y;
+    return U;
+}
+
+
+ostream & operator<<(ostream & out, Vector & V)
+{
+    for (Vector::iterator itr= V.begin(); itr != V.end(); itr++)
+        out << *itr << " ";
+    out << endl;
+    return out;
+}
+
+ostream & operator<<(ostream & out, vector<int> & V)
+{
+    for (vector<int>::iterator itr= V.begin(); itr != V.end(); itr++)
+        out << *itr << " ";
+    out << endl;
+    return out;
+}
